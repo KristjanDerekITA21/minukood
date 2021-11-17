ed56548265d1f1b65cb4e012262ce4c2643dbb7b
KristjanDerekITA21 <94574134+KristjanDerekITA21@users.noreply.github.com>
2021-11-18 00:39:16 +0200

    Add files via upload

---
+++ too.py
@@ -0,0 +1,11 @@
+def paranda_komavead(lause):
+    
+    lause = lause.replace(" sest ", ", sest ")
+    lause = lause.replace(" kuid ", ", kuid ")
+    lause = lause.replace(" et ", ", et ")
+    lause = lause.replace(" vaid ", ", vaid ")
+    print(lause)
+    
+paranda_komavead("läksin poodi et leiba tuua kuid leib oli kahjuks otsas")
+
+    
